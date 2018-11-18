87f44d090e0b4ad768e29d69238e742066c7e2c4
go2carter <cnb47@cornell.edu>
2018-11-18 01:46:26 -0800

    some server routes

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
 from firebase_config import firebase
+from requests.exceptions import HTTPError
+
+db = firebase.database()
+auth = firebase.auth()
 
 app = Flask(__name__)
 
@@ -7,6 +13,39 @@ app = Flask(__name__)
 def index():
     return 'Hello, World!'
 
-@app.route('/test_push/')
-def test_push():
-    return
+@app.route('/hello/<name>', methods=['GET', 'POST'])
+def hello_name(name=None):
+    return 'Hello, %s' % name
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            user_data = auth.sign_in_with_email_and_password(email, password)
+        except HTTPError as e:
+            user_data = {'localID': ''}
+        return jsonify(user_data)
+
+@app.route('/user_info', methods=['POST'])
+def user_info():
+    if request.method == 'POST':
+        user_id = request.form['localID']
+        user_info = db.child('users').child(user_id).get()
+        return jsonify(user_info.val())
+
+@app.route('/make_post_image', methods=['POST'])
+def make_post():
+    if request.method == 'POST':
+        media = 
+
+@app.route('/make_post_video', methods=['POST'])
+
+
+@app.route('/post_comment', methods=['POST'])
+def post_comment():
+    if request.method == 'POST':
+        comment_text = request.form['comment_text']
+        timestamp = request.form['timestamp']
+        user_id = request.form['localID']
--- backend/firebase_config.py
+++ backend/firebase_config.py
@@ -1,12 +1,12 @@
 import pyrebase
 
 config = {
-    apiKey: "AIzaSyB-qKMHUdMC4w0QcZIzmVsjFWJmTrDB_wY",
-    authDomain: "sachacks-2018.firebaseapp.com",
-    databaseURL: "https://sachacks-2018.firebaseio.com",
-    projectId: "sachacks-2018",
-    storageBucket: "sachacks-2018.appspot.com",
-    messagingSenderId: "23141275388"
+    "apiKey": "AIzaSyB-qKMHUdMC4w0QcZIzmVsjFWJmTrDB_wY",
+    "authDomain": "sachacks-2018.firebaseapp.com",
+    "databaseURL": "https://sachacks-2018.firebaseio.com",
+    "projectId": "sachacks-2018",
+    "storageBucket": "sachacks-2018.appspot.com",
+    "messagingSenderId": "23141275388"
 }
 
 firebase = pyrebase.initialize_app(config)
